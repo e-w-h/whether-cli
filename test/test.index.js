56e3affbdf27a4f953ed545cf5ea475736f715f9
E H <erichou24@gmail.com>
2021-03-31 23:57:01 -0400

    add tests for forecast

--- test/test.index.js
+++ test/test.index.js
@@ -7,6 +7,7 @@ describe('Consuming Weather API', function() {
     const country = 'us'
     const API_key = 'd3c15055294886db5cbaba813c393d87'
     let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${country}&appid=${API_key}&units=imperial`
+    
     it('returns status 200', async function() {
       const res = await fetch(url)
       expect(res.status).to.equal(200)
@@ -25,6 +26,24 @@ describe('Consuming Weather API', function() {
       const data = await res.json()
       expect(data).to.have.property('main')
       expect(data.main).to.have.property('temp')
+    })
+  })
+
+  describe('Getting forecast data', function() {
+    let lat = 40.7685
+    let lon = -73.9588
+    const API_key = 'd3c15055294886db5cbaba813c393d87'
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,daily,minutely,alerts&appid=${API_key}&units=imperial`
+    
+    it('returns status 200', async function() {
+      const res = await fetch(url)
+      expect(res.status).to.equal(200)
+    })
+
+    it('contains hourly information in the body', async function() {
+      const res = await fetch(url)
+      const data = await res.json()
+      expect(data).to.have.property('hourly')
     })
   })
 })
