a08ad58b5ceffc005287356cf997de878573c0b4
E H <erichou24@gmail.com>
2021-04-02 17:11:31 -0400

    test: database actions

---
+++ test/sqlite.spec.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect
+const db = require('../bin/sqlite')
+
+describe('Database actions', function() {
+  it('initializes correctly', async function() {
+    await db.init()
+  })
+
+  it('can store and retrieve items', async function() {
+    const LOCATION = {
+      id: 0,
+      zipcode: '10021',
+      country_code: 'us',
+    }
+      
+    await db.init()
+    await db.addLoc(LOCATION)
+    const locs = await db.getLocs()
+    expect(locs.length).to.equal(1)
+    expect(locs[0]).to.deep.equal(LOCATION)
+  })
+})
